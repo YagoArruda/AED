9f97cf1b59cf7d81218954493ac7dec8b9b8d1ae
YagoArruda <yago4444@gmail.com>
2023-10-07 18:45:18 -0300

    7/10

--- MergeSort/Program.cs
+++ MergeSort/Program.cs
@@ -2,11 +2,19 @@
 using System.IO;
 using System.Text;
 
+using System.Diagnostics;
+
 class Program
 {
     public static void Main(string[] args)
     {
-        int[] vet = { 23, 15, 6, 9, 13 };
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        //Gerar um vetor Aleatorio
+        int tamanho = 50;
+        int[] vet = new int[tamanho];
+        gerarVetor(tamanho, vet);
 
         mergeSort(0, vet.Length, vet);
 
@@ -14,51 +22,55 @@ class Program
         {
             Console.WriteLine(vet[i]);
         }
+
+        stopwatch.Stop();
+        long tempoDecorridoMs = stopwatch.ElapsedMilliseconds;
+        Console.WriteLine("Tempo de execução: " + tempoDecorridoMs + " milissegundos");
     }
 
     public static void mergeSort(int inicio, int fim, int[] vetor)
     {
-        int meio = (inicio + fim) / 2;
+        if (fim > inicio)
+        {
+            int meio = (inicio + fim) / 2;
 
-        if(meio == 0){
-            return;
+            mergeSort(0, meio, vetor);
+            mergeSort(meio + 1, fim, vetor);
+
+            intercalar(vetor, inicio, fim);
         }
-
-        mergeSort(0, meio, vetor);
-        mergeSort(meio + 1, fim, vetor);
-
-        intercalar(vetor, inicio, fim);
-
-        return;
     }
 
     public static void intercalar(int[] vetor, int inicio, int fim)
     {
+        //Selection Sort n2-1
         int aux;
-        //somente para teste 
+
         for (int i = 0; i < fim - 1; i++)
         {
+            int index = i;
+
             for (int j = i; j < fim; j++)
             {
-                if (vetor[i] > vetor[j])
+                if (vetor[index] > vetor[j])
                 {
-                    aux = vetor[i];
-                    vetor[i] = vetor[j];
-                    vetor[j] = aux;
+                    index = j;
                 }
             }
+
+            aux = vetor[i];
+            vetor[i] = vetor[index];
+            vetor[index] = aux;
         }
     }
 
+    public static void gerarVetor(int tamanho, int[] vetor)
+    {
+        Random rand = new Random();
 
-    /*
-        public static void subInt(int inicio, int fim,int[] novo, int[] original){
-            for(int i = 0; i < fim; i++){
-                novo[i] = original[i];
-            }
+        for (int i = 0; i < tamanho; i++)
+        {
+            vetor[i] = rand.Next(0, 100);
         }
-
-        */
-
-
+    }
 }
