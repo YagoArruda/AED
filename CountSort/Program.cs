7332b79450945d9360a013e5bd049823fc8d7616
YagoArruda <yago4444@gmail.com>
2023-10-07 19:51:19 -0300

    FNL 2 - CountSort

--- CountSort/Program.cs
+++ CountSort/Program.cs
@@ -1,80 +1,84 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using System.Diagnostics;
 
 class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine($"inicio");
-
-        int tam = 4;
+        //Inicializa os vetores com os valores originais e o dos ordenados
+        int tam = 500;
         int[] baseDeDados = new int[tam];
         int[] baseOrdenada = new int[tam];
-        //int[] contagem = new int[tam];
 
-        baseDeDados[0] = 1;
-        baseDeDados[1] = 5;
-        baseDeDados[2] = 9;
-        baseDeDados[3] = 4;
-        //gerarVetor(baseDeDados);
+        //Preenche o vetor 
+        gerarVetor(baseDeDados);
+
+        //Inicia a contagem do tempo de execução
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        //Determina o maior elemento do vetor
         int maior = baseDeDados[0];
         for (int i = 1; i < baseDeDados.Length; i++)
         {
             if (baseDeDados[i] > maior)
             {
                 maior = baseDeDados[i];
-                Console.WriteLine($"maior:{maior}");
             }
         }
+
+        //Inicializa os vetores de contagem e posição
         maior++;
         int[] contagem = new int[maior];
         int[] pos = new int[maior];
 
+        //Conta quantas vezes cada valor aparece
         for (int i = 0; i < baseDeDados.Length; i++)
         {
             contagem[baseDeDados[i]] += 1;
         }
-        for (int i = 0; i < contagem.Length; i++)
-        {
-            Console.WriteLine($"{i}/{contagem[i]}");
-        }
-        Console.WriteLine($"---Fim-Contagem---");
 
+        //Calcula a posição de cada valor
         int somador = 0;
         for (int i = 0; i < pos.Length; i++)
         {
-
             pos[i] = somador;
 
             somador += contagem[i];
-            //pos[contagem[i]] += 1;
         }
-        for (int i = 0; i < pos.Length; i++)
-        {
-            Console.WriteLine($"{i}/{pos[i]}");
-        }
-        Console.WriteLine($"---Fim-Pos---");
 
+        //Coloca os valores em suas posições de acordo com a posição (Ordena)
         for (int i = 0; i < baseDeDados.Length; i++)
         {
-            //baseOrdenada[i] = baseDeDados[pos[i]];
             baseOrdenada[pos[baseDeDados[i]]] = baseDeDados[i];
             pos[baseDeDados[i]] += 1;
         }
+
+        //Finaliza a contagem do tempo de execução
+        stopwatch.Stop();
+        long tempoDecorridoMs = stopwatch.ElapsedMilliseconds;
+
+        //Imprime o vetor original
+        for (int i = 0; i < baseDeDados.Length; i++)
+        {
+            Console.Write($"{baseDeDados[i]}/");
+        }
+        Console.WriteLine();
+
+        //Imprime o vetor ordenado
         for (int i = 0; i < baseOrdenada.Length; i++)
         {
-            Console.WriteLine($"{i}/{baseOrdenada[i]}");
+            Console.Write($"{baseOrdenada[i]}/");
         }
-        /*
-                for (int i = 0; i < baseDeDados.Length; i++)
-                {
-                    Console.Write($"{contagem[i]} / {pos[contagem[i]]}");
-                }
-        */
-        Console.WriteLine($"fim");
+        Console.WriteLine();
+
+        //Imprime o tempo de execução
+        Console.WriteLine("Tempo de execução: " + tempoDecorridoMs + " milissegundos");
     }
 
+    //Preenche o vetor recebido até o seu limite
     public static void gerarVetor(int[] vetor)
     {
 
@@ -82,7 +86,7 @@ class Program
 
         for (int i = 0; i < vetor.Length; i++)
         {
-            vetor[i] = rd.Next(1, 75);
+            vetor[i] = rd.Next(1, 76);
         }
 
     }
