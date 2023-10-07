5229905acf8a7211af76b28388f8e3c18a2d3571
YagoArruda <yago4444@gmail.com>
2023-10-07 19:37:27 -0300

    FNL 1 - CountSort

--- CountSort/Program.cs
+++ CountSort/Program.cs
@@ -6,21 +6,73 @@ class Program
 {
     public static void Main(string[] args)
     {
-        int tam = 2;
+        Console.WriteLine($"inicio");
+
+        int tam = 4;
         int[] baseDeDados = new int[tam];
-        int[] contagem = new int[tam];
-gerarVetor(baseDeDados);
+        int[] baseOrdenada = new int[tam];
+        //int[] contagem = new int[tam];
 
-        for (int i = 0; i < baseDeDados.Length + 1; i++)
+        baseDeDados[0] = 1;
+        baseDeDados[1] = 5;
+        baseDeDados[2] = 9;
+        baseDeDados[3] = 4;
+        //gerarVetor(baseDeDados);
+        int maior = baseDeDados[0];
+        for (int i = 1; i < baseDeDados.Length; i++)
         {
-            contagem[i--] += 1;
+            if (baseDeDados[i] > maior)
+            {
+                maior = baseDeDados[i];
+                Console.WriteLine($"maior:{maior}");
+            }
         }
+        maior++;
+        int[] contagem = new int[maior];
+        int[] pos = new int[maior];
 
-        for (int i = 0; i < baseDeDados.Length + 1; i++)
+        for (int i = 0; i < baseDeDados.Length; i++)
         {
-            Console.Write($"{baseDeDados[i]} /");
+            contagem[baseDeDados[i]] += 1;
         }
+        for (int i = 0; i < contagem.Length; i++)
+        {
+            Console.WriteLine($"{i}/{contagem[i]}");
+        }
+        Console.WriteLine($"---Fim-Contagem---");
 
+        int somador = 0;
+        for (int i = 0; i < pos.Length; i++)
+        {
+
+            pos[i] = somador;
+
+            somador += contagem[i];
+            //pos[contagem[i]] += 1;
+        }
+        for (int i = 0; i < pos.Length; i++)
+        {
+            Console.WriteLine($"{i}/{pos[i]}");
+        }
+        Console.WriteLine($"---Fim-Pos---");
+
+        for (int i = 0; i < baseDeDados.Length; i++)
+        {
+            //baseOrdenada[i] = baseDeDados[pos[i]];
+            baseOrdenada[pos[baseDeDados[i]]] = baseDeDados[i];
+            pos[baseDeDados[i]] += 1;
+        }
+        for (int i = 0; i < baseOrdenada.Length; i++)
+        {
+            Console.WriteLine($"{i}/{baseOrdenada[i]}");
+        }
+        /*
+                for (int i = 0; i < baseDeDados.Length; i++)
+                {
+                    Console.Write($"{contagem[i]} / {pos[contagem[i]]}");
+                }
+        */
+        Console.WriteLine($"fim");
     }
 
     public static void gerarVetor(int[] vetor)
@@ -30,7 +82,7 @@ gerarVetor(baseDeDados);
 
         for (int i = 0; i < vetor.Length; i++)
         {
-            vetor[i] = rd.Next(1,75);
+            vetor[i] = rd.Next(1, 75);
         }
 
     }
