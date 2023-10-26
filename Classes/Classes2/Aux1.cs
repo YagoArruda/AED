3a7689664d3ca34debec39401f70093433f0b71a
YagoArruda <yago4444@gmail.com>
2023-10-26 17:55:04 -0300

    Introdução a classes

---
+++ Classes/Classes2/Aux1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class teste
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    teste oi;
+    public static void Main(string[] args)
+    {
+
+    }
+}
+
+public class teste{
+    
+}
