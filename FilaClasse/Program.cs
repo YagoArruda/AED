9d7f5aa6f9d7f5754809a8b32d71fc49099ce73b
YagoArruda <yago4444@gmail.com>
2023-10-27 11:23:35 -0300

    Fila Com Classe Completa

--- FilaClasse/Program.cs
+++ FilaClasse/Program.cs
@@ -5,6 +5,8 @@ class Program
 {
     public static void Main(string[] args)
     {
+        Console.Clear();
+
         fila teste = new fila();
 
         int op = 0;
@@ -42,23 +44,26 @@ public class fila
 
     public void remover()
     {
-        for (int i = 0; i < this.qtd - 1; i++)
+        bool state = this.vazio();
+        if (state == false)
         {
-            this.dados[i] = this.dados[i + 1];
+            for (int i = 0; i < this.qtd - 1; i++)
+            {
+                this.dados[i] = this.dados[i + 1];
+            }
+            this.qtd--;
+            Console.WriteLine("Primeiro da fila removido");
         }
-        this.qtd--;
     }
 
-    public void vazio()
+    public bool vazio()
     {
         if (this.qtd == 0)
         {
             Console.WriteLine("Fila Vazia!");
+            return true;
         }
-        else
-        {
-            Console.WriteLine("Nao esta Vazia!");
-        }
+        return false;
     }
 
     public bool cheio()
@@ -82,7 +87,10 @@ public class fila
         int op = 0;
         do
         {
+            Console.Clear();
+
             Console.WriteLine("--- Fila ---");
+            Console.WriteLine($"{this.qtd}/{this.dados.Length}");
             Console.WriteLine("1 - Inserir");
             Console.WriteLine("2 - Remover");
             Console.WriteLine("3 - Verificar vazio");
@@ -90,7 +98,7 @@ public class fila
             Console.WriteLine("0 - sair");
             op = int.Parse(Console.ReadLine());
         }
-        while (op > 5 || op < 0);
+        while (op > 4 || op < 0);
 
         this.escolha(op);
 
@@ -108,12 +116,25 @@ public class fila
                 this.remover();
                 break;
             case 3:
-                this.vazio();
+                bool state = this.vazio();
+                if (state == false)
+                {
+                    Console.WriteLine("Nao esta vazio!");
+                }
                 break;
             case 4:
                 this.ler();
                 break;
         }
+
+        if(op != 1 && op != 0){
+            this.esperarPressionar();
+        }
     }
 
+    public void esperarPressionar()
+    {
+        Console.WriteLine("Pressione qualquer tecla para continuar...");
+        Console.ReadKey();
+    }
 }
